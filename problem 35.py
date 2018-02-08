b0b25bf2de119948c842fe5f0839054d40610933
AkinaRozu01 <35246208+AkinaRozu01@users.noreply.github.com>
2018-02-08 13:11:08 -0500

    Update problem 35.py

--- problem 35.py
+++ problem 35.py
@@ -1,22 +1,21 @@
 #list approach
-import math
-number = 100
-sqrt = math.sqrt(number)
+def __init__(self, digit):
+    self.a=digita
+        
+def switch(self):
+    self.a
+#l += [l.pop(0)] moves things to the end of a list
+#make a loop!!!
+len
+val= 20
+number =list(str(val))
+print number
+number +=[number.pop(0)]
+print number
+number +=[number.pop(0)]
+print number
+print numbers
 
-numbers=[]
-i=1
-for digit in range (1,100):
-    numbers.append(digit)
-print numbers
-for digit in numbers:
-    is_prime=True
-    for x in range (int(math.ceil(math.sqrt(13195)))+1):
-        if x>1:
-            if  13195 % x==0:
-                is_prime=False
-            if is_prime==False:
-                numbers.remove(x)
-print numbers
 #object approach
 import math
 class Number:
