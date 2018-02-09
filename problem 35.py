a31dd8ff990e2b5f52059ed9e2376e2ee8465843
AkinaRozu01 <35246208+AkinaRozu01@users.noreply.github.com>
2018-02-09 13:10:55 -0500

    Update problem 35.py

--- problem 35.py
+++ problem 35.py
@@ -25,3 +25,24 @@ class Number:
         
     def switch(self):
         self.a
+#from random import randint
+
+#val = randint(2, 4000000)
+#number = list(str(val))
+#print number
+
+#for i in range(len(number) - 1):
+ #   number += [number.pop(0)]
+  #  print "".join(number)
+import math
+number = 1000000
+sqrt = math.sqrt(number)
+prime=[]
+i=1
+while i <100000:
+    is_prime = True
+    prime.append(i)
+    for digit in range (2,int(math.ceil(math.sqrt(i)) + 1)):
+            if i % digit == 0:
+                prime.remove(i)
+print prime
